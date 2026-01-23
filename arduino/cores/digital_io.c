51cf694972512c05c3d9c09e17cd5526325c46dc
Amalia Wahyu <uamaliawahyu@gmail.com>
2026-01-23 10:30:37 +0700

    add digital i/o functions based on arduino language reference document

---
+++ arduino/cores/digital_io.c
@@ -0,0 +1,18 @@
+#include "digital_io.h"
+
+int digitalRead(int pin){
+
+    if (){
+        return HIGH;
+    } else {
+        return LOW;
+    }
+}
+
+void digitalWrite(int pin, int value){
+
+}
+
+void pinMode(int pin, uint8_t mode){
+
+}
---
+++ arduino/cores/digital_io.h
@@ -0,0 +1,5 @@
+ #include <stdint.h>
+ 
+ int digitalRead(int pin);
+ void digitalWrite(int pin, int value);
+ void pinMode(int pin, uint8_t mode);
