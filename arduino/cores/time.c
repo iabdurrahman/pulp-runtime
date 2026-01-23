e69b3aa2846d30d8b81bc3cc1f74bebb88e1f664
Alfi Dzikro Hasibuan <alfidhsb@gmail.com>
2026-01-23 10:34:18 +0700

    add .c and .h file for time api, .c still contain empty functions

---
+++ arduino/cores/time.c
@@ -0,0 +1,17 @@
+#include "time.h"
+
+void delay(unsigned long ms) {
+
+}
+
+void delayMicroseconds(unsigned int us) {
+
+}
+
+unsigned long millis(void) {
+
+}
+
+unsigned long micros(void) {
+    
+}
---
+++ arduino/cores/time.h
@@ -0,0 +1,12 @@
+#ifndef time_h
+#define time_h
+
+void delay(unsigned long ms);
+
+void delayMicroseconds(unsigned int us);
+
+unsigned long millis(void);
+
+unsigned long micros(void);
+
+#endif
