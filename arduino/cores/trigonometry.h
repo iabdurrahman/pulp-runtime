c0f84b6be259f602da84052273866facb5d5443c
Alfi Dzikro Hasibuan <alfidhsb@gmail.com>
2026-01-23 10:23:13 +0700

    add trigonometry .h file

---
+++ arduino/cores/trigonometry.h
@@ -0,0 +1,10 @@
+#ifndef trigonometry_h
+#define trigonometry_h
+
+#include <math.h>
+
+/*cos() : already defined in math.h*/
+/*sin() : already defined in math.h*/
+/*tan() : already defined in math.h*/
+
+#endif
