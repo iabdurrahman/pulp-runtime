2b5798c1fd3a00fbacb0eea3a6c7f8cdde31cdac
Alfi Dzikro Hasibuan <alfidhsb@gmail.com>
2026-02-02 13:34:45 +0700

    add functions defenition in randomNumbers.c

--- arduino/cores/randomNumbers.c
+++ arduino/cores/randomNumbers.c
@@ -1,13 +1,23 @@
-
+#include "stdlib.h"
+#include <math.h>
 
 long random(long max) {
-
+    if (max == 0) {
+        return 0;
+    }
+    return random() % max
 }
 
 long random(long min, long max) {
-
+    if (min >= max) {
+        return min;
+    }
+    long diff = max - min;
+    return random(diff) + min;
 }
 
 void randomSeed(unsigned long seed) {
-
+    if (seed != 0) {
+        random(seed);
+    }
 }
