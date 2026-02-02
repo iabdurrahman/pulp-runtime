c3386f48a662f92343908b59c5b275926f177bf3
Alfi Dzikro Hasibuan <alfidhsb@gmail.com>
2026-02-02 13:36:34 +0700

    added functions definition in time.c

--- arduino/cores/time.c
+++ arduino/cores/time.c
@@ -1,17 +1,19 @@
 #include "time.h"
+#include "implem/delay.h"
+#include "implem/tick.h"
 
 void delay(unsigned long ms) {
-
+    pos_delay_busy_ms((long) ms);
 }
 
 void delayMicroseconds(unsigned int us) {
-
+    pos_delay_busy_us((long) us);
 }
 
 unsigned long millis(void) {
-
+    return (unsigned long) pos_tick_get_counter_ms();
 }
 
 unsigned long micros(void) {
-    
+    return (unsigned long) pos_tick_get_counter_us();   
 }
